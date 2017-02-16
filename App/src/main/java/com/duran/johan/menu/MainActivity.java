32fd93aef139f2495cb9a6a5cd23eddfe3765489
Johan Durán Cerdas <johan.durancerdas@gmail.com>
2017-02-16 00:10:13 -0600

    se cargan y muestran los marcadores en el mapa

--- App/src/main/java/com/duran/johan/menu/MainActivity.java
+++ App/src/main/java/com/duran/johan/menu/MainActivity.java
@@ -1,6 +1,7 @@
 package com.duran.johan.menu;
 
 import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -32,8 +33,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Console;
 
 import static android.R.attr.id;
+import static com.duran.johan.menu.R.id.map;
+import static com.duran.johan.menu.R.layout.maps;
 
 public class MainActivity extends Navigation
         implements OnMapReadyCallback {
@@ -43,9 +50,8 @@ public class MainActivity extends Navigation
 
     //variables para peticiones al servidor
     MySingleton singleton;
-    String server="http://192.168.0.103:8081/";
-    String dir = "Proyectos/Proyecto_Muestreo_Agua/paginaBusqueda/php/";
-    String file = "MapReduceMR.php"; //temporal solo de ejemplo.
+    String server="http://192.168.0.106:8081/";
+    String dir = "Proyectos/Monitoreo_Agua_Web/php/";
 
     //control de multidex.
     @Override
@@ -58,25 +64,30 @@ public class MainActivity extends Navigation
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         RelativeLayout item = (RelativeLayout)findViewById(R.id.relative_element);
-        View child = getLayoutInflater().inflate(R.layout.maps, null);
+        View child = getLayoutInflater().inflate(maps, null);
         item.addView(child);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(map);
+        mapFragment.getMapAsync(this);
     }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng costaRica = new LatLng(10.131581, -84.181927);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(costaRica));
+        String file = "getMarkers_busqueda.php"; //temporal solo de ejemplo.
+        getRequest(file,1);
     }
 
 
     //Método utilizado para realizar peticiones asincronas al servidor
-    public void getRequest(String dir,final int num) {
-        dir = server+dir;
+    public void getRequest(String file,final int num) {
+        dir = server+dir+file;
         JsonArrayRequest jsArrRequest = new JsonArrayRequest
                 (Request.Method.GET, dir, null, new Response.Listener<JSONArray>() {
 
@@ -85,6 +96,25 @@ public class MainActivity extends Navigation
                         switch (num){//Incluir los casos dependiendo de la cantidad de llamados distintos que se puedan hacer.
                             case 1:
                                 //lo que se desea hacer para la petición 1
+                                int longitud =0;
+                                JSONObject resultados=new JSONObject();
+                                try {
+                                    longitud = response.getJSONObject(0).getJSONObject("results").length();
+                                    resultados=response.getJSONObject(0).getJSONObject("results");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                                for (int i = 0;i<longitud;i++){
+                                    try {
+                                        JSONObject location=resultados.getJSONObject(Integer.toString(i)).getJSONObject("value").getJSONObject("location");
+                                        LatLng position= new LatLng(location.getDouble("lat"),location.getDouble("lng"));
+                                        mMap.addMarker(new MarkerOptions().position(position).title("Centro de Costa Rica"));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+
                                 break;
                             case 2:
                                 //lo que se desea hacer para la petición 2
